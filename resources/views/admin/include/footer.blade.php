29d760f756502ab47e525982a12b7b971fdacb07
Arun <aroonkumar92@gmail.com>
2018-09-26 08:19:44 +0530

    Toggle menu for admin issue

--- resources/views/admin/include/footer.blade.php
+++ resources/views/admin/include/footer.blade.php
@@ -2,36 +2,36 @@
 		var resizefunc = [];
 	</script>
 	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-	<script src="{{BASE_URL}}public/assets/libs/jquery/jquery-1.11.1.min.js"></script>
-	<script src="{{BASE_URL}}public/assets/libs/bootstrap/js/bootstrap.min.js"></script>
-	<script src="{{BASE_URL}}public/assets/libs/jqueryui/jquery-ui-1.10.4.custom.min.js"></script>
-	<script src="{{BASE_URL}}public/assets/libs/jquery-ui-touch/jquery.ui.touch-punch.min.js"></script>
+	<script src="{{asset('public/assets/libs/jquery/jquery-1.11.1.min.js')}}"></script>
+	<script src="{{asset('public/assets/libs/bootstrap/js/bootstrap.min.js')}}"></script>
+	<script src="{{asset('public/assets/libs/jqueryui/jquery-ui-1.10.4.custom.min.js')}}"></script>
+	<script src="{{asset('public/assets/libs/jquery-ui-touch/jquery.ui.touch-punch.min.js')}}"></script>
 	
-	<script src="{{BASE_URL}}public/assets/libs/jquery-animate-numbers/jquery.animateNumbers.js"></script>
-	<script src="{{BASE_URL}}public/assets/libs/ios7-switch/ios7.switch.js"></script>
-	<script src="{{BASE_URL}}public/assets/libs/fastclick/js/fastclick.js"></script>
-	<script src="{{BASE_URL}}public/assets/libs/jquery-blockui/jquery.blockUI.js"></script>
-	<script src="{{BASE_URL}}public/assets/libs/bootstrap-bootbox/bootbox.min.js"></script>
-	<script src="{{BASE_URL}}public/assets/libs/jquery-slimscroll/jquery.slimscroll.js"></script>
-	<script src="{{BASE_URL}}public/assets/libs/jquery-sparkline/jquery-sparkline.js"></script>
-	<script src="{{BASE_URL}}public/assets/libs/nifty-modal/js/classie.js"></script>
-	<script src="{{BASE_URL}}public/assets/libs/nifty-modal/js/modalEffects.js"></script>
-	<script src="{{BASE_URL}}public/assets/libs/sortable/sortable.min.js"></script>
-	<script src="{{BASE_URL}}public/assets/libs/bootstrap-fileinput/bootstrap.file-input.js"></script>
-	<script src="{{BASE_URL}}public/assets/libs/bootstrap-select/bootstrap-select.min.js"></script>
-	<script src="{{BASE_URL}}public/assets/libs/bootstrap-select2/select2.min.js"></script>
-	<script src="{{BASE_URL}}public/assets/libs/magnific-popup/jquery.magnific-popup.min.js"></script> 
-	<script src="{{BASE_URL}}public/assets/libs/pace/pace.min.js"></script>
-	<script src="{{BASE_URL}}public/assets/libs/bootstrap-datepicker/js/bootstrap-datepicker.js"></script>
-	<script src="{{BASE_URL}}public/assets/libs/jquery-icheck/icheck.min.js"></script>
+	<script src="{{asset('public/assets/libs/jquery-animate-numbers/jquery.animateNumbers.js')}}"></script>
+	<script src="{{asset('public/assets/libs/ios7-switch/ios7.switch.js')}}"></script>
+	<script src="{{asset('public/assets/libs/fastclick/js/fastclick.js')}}"></script>
+	<script src="{{asset('public/assets/libs/jquery-blockui/jquery.blockUI.js')}}"></script>
+	<script src="{{asset('public/assets/libs/bootstrap-bootbox/bootbox.min.js')}}"></script>
+	<script src="{{asset('public/assets/libs/jquery-slimscroll/jquery.slimscroll.js')}}"></script>
+	<script src="{{asset('public/assets/libs/jquery-sparkline/jquery-sparkline.js')}}"></script>
+	<script src="{{asset('public/assets/libs/nifty-modal/js/classie.js')}}"></script>
+	<script src="{{asset('public/assets/libs/nifty-modal/js/modalEffects.js')}}"></script>
+	<script src="{{asset('public/assets/libs/sortable/sortable.min.js')}}"></script>
+	<script src="{{asset('public/assets/libs/bootstrap-fileinput/bootstrap.file-input.js')}}"></script>
+	<script src="{{asset('public/assets/libs/bootstrap-select/bootstrap-select.min.js')}}"></script>
+	<script src="{{asset('public/assets/libs/bootstrap-select2/select2.min.js')}}"></script>
+	<script src="{{asset('public/assets/libs/magnific-popup/jquery.magnific-popup.min.js')}}"></script> 
+	<script src="{{asset('public/assets/libs/pace/pace.min.js')}}"></script>
+	<script src="{{asset('public/assets/libs/bootstrap-datepicker/js/bootstrap-datepicker.js')}}"></script>
+	<script src="{{asset('public/assets/libs/jquery-icheck/icheck.min.js')}}"></script>
 
 	<!-- Demo Specific JS Libraries -->
-	<script src="{{BASE_URL}}public/assets/libs/prettify/prettify.js"></script>
+	<script src="{{asset('public/assets/libs/prettify/prettify.js')}}"></script>
 
-	<script src="{{BASE_URL}}public/assets/js/init.js"></script>
+	<script src="{{asset('public/assets/js/init.js')}}"></script>
     <!-- Page Specific JS Libraries -->
-	<script src="{{BASE_URL}}public/assets/libs/fullcalendar/fullcalendar.min.js"></script>
-	<script src="{{BASE_URL}}public/assets/js/pages/calendar.js"></script>
+	<script src="{{asset('public/assets/libs/fullcalendar/fullcalendar.min.js')}}"></script>
+	<script src="{{asset('public/assets/js/pages/calendar.js')}}"></script>
 	</body>
 
 <!-- Mirrored from themesground.com/nexus-admin/template/HTML/calendar.html by HTTrack Website Copier/3.x [XR&CO'2014], Sat, 13 Jan 2018 18:02:37 GMT -->
