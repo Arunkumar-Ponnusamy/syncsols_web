6245086ee287f19c900baea389f6c18e1bac8b33
Chitra <chitra.anusya94@gmail.com>
2019-09-30 00:42:07 +0530

    header and career new section

---
+++ app/Career.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Career extends Model
+{
+    
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     **/
+    protected $fillable = [
+        'id',
+        'content',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'created_at', 'updated_at'
+    ];
+
+    /**
+     * The services that belong to the user.
+     */
+    // public function provider()
+    // {
+    //     return $this->belongsTo('App\User');
+    // }
+
+}
